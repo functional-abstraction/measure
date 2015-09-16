0e48a3d7b8b6e6476f79f1df08a4b9897ce0d260
aureooms <aurelien.ooms@gmail.com>
2015-09-16 20:02:16 +0000

    key

--- js/src/1-api/0-Monoids.js
+++ js/src/1-api/0-Monoids.js
@@ -4,5 +4,6 @@ const Monoids = { } ;
 Monoids.INTEGER_ADD = new IntegerAdd( ) ;
 Monoids.INTEGER_MUL = new IntegerMul( ) ;
 Monoids.INTEGER_MAX = new IntegerMax( ) ;
+Monoids.KEY = new KeyMonoid( ) ;
 
 exports.Monoids = Monoids ;
--- js/src/1-api/1-Measures.js
+++ js/src/1-api/1-Measures.js
@@ -3,5 +3,6 @@ const Measures = { } ;
 
 Measures.COUNTER = new Counter( ) ;
 Measures.MAX = new Max( ) ;
+Measures.KEY = new KeyMeasure( ) ;
 
 exports.Measures = Measures ;
