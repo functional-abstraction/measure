5c24be5f0032bf08f8f9812ab0d9d2a1b158ba16
aureooms <aurelien.ooms@gmail.com>
2015-09-17 09:38:05 +0000

    aliases

--- js/src/1-api/1-Measures.js
+++ js/src/1-api/1-Measures.js
@@ -6,4 +6,7 @@ Measures.MAX = new Max( ) ;
 Measures.KEY = new KeyMeasure( ) ;
 Measures.INTERVAL = new Interval( ) ;
 
+Measures.PRIO = Measures.MAX ;
+Measures.SIZE = Measures.COUNTER ;
+
 exports.Measures = Measures ;
