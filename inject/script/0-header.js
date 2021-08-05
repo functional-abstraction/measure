dfbb0ad15d31a486311752adbdb27b0aa1807716
make-github-pseudonymous-again <make-github-pseudonymous-again@users.noreply.github.com>
2021-08-05 08:25:23 +0000

    Deploying to gh-pages from @ functional-abstraction/measure@9e6a0af5e9616eab11094857a0144a1b823eabe7 🚀

--- inject/script/0-header.js
+++ inject/script/0-header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/functional-abstraction/measure';
+	testlink.href = 'https://app.codecov.io/gh/functional-abstraction/measure';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
